4c919fef68635034f9c75d4addb57dcbc970b19e
lpeppe <lcpp1994@gmail.com>
2017-06-17 12:28:37 +0200

    minor bugfix

--- app2.js
+++ app2.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
-var cors = require('cors');
-app.use(cors());
+// var cors = require('cors');
+//app.use(cors());
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.get('/', function(req, res) {
   var start = new Date().getTime();
   var webdriver = require('selenium-webdriver'),
@@ -79,6 +84,6 @@ app.get('/', function(req, res) {
 });
 
 
-app.listen(3001, function() {
-  console.log('Example app listening on port 3001!');
+app.listen(8080, function() {
+  console.log('Example app listening on port 8080!');
 });
