ff5688099db59c953c8a2d92abad366e5aa82660
lpeppe <lcpp1994@gmail.com>
2017-06-18 11:26:50 +0200

    added CORS

--- qcScraper.js
+++ qcScraper.js
@@ -8,7 +8,11 @@ var driver = new webdriver.Builder()
   .forBrowser('phantomjs')
   .build();
 var app = express();
-
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.get('/', function(req, res) {
   var start = new Date().getTime();
   var toReturn = [];
