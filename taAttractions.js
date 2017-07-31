cca2cf4c2b7f28ffcd4c66c109608a583a8c0c63
lpeppe <lcpp1994@gmail.com>
2017-07-31 12:30:07 +0200

    Added attractions without images

--- taAttractions.js
+++ taAttractions.js
@@ -22,11 +22,8 @@ app.get('/', function(req, res) {
   driver.findElement(By.className('typeahead_input')).sendKeys(req.query.loc);
   driver.findElement(By.id('SUBMIT_THINGS_TO_DO')).click();
   driver.wait(until.elementLocated(By.id('HEADING')), 8000)
-  // driver.wait(until.stalenessOf(driver.findElement(By.css("img[src='https://static.tacdn.com/img2/x.gif']"))))
     .then(_ => {
       driver.sleep(1000)
-      //"//div[@class='listing_details' and not(div[@class='photo_booking']//div[@class='noImageBorder']//img[@class='npp'])]"
-      //"//div[@class='listing_details' and not(div[@class='photo_booking']//div[@class='noImageBorder']//img[@class='npp']) and div[@class='listing_info']/div[@class='tag_line']/div/a]"
       driver.findElements(By.xpath("//div[@class='listing_details' and not(div[@class='photo_booking']//div[@class='noImageBorder']//img[@class='npp']) and div[@class='listing_info']/div[@class='tag_line']/div/a]"))
         .then(elements => {
           for (var i in elements) {
@@ -45,13 +42,27 @@ app.get('/', function(req, res) {
               })
           }
         })
-        .then(_ => {
-          res.json(toReturn)
-          driver.quit();
-          var end = new Date().getTime();
-          var time = end - start;
-          console.log('Execution time: ' + time);
+      driver.findElements(By.xpath("//div[@class='listing_details' and div[@class='photo_booking']//div[@class='noImageBorder'] and div[@class='listing_info']/div[@class='tag_line']/div/a]"))
+        .then(elements => {
+          for (var i in elements) {
+            var p1 = elements[i].findElement(By.css("div.listing_title ")).getText();
+            var p2 = elements[i].findElement(By.css("div.tag_line span")).getText();
+            Promise.all([p1, p2])
+              .then(values => {
+                toReturn.push({
+                  nome: values[0],
+                  tipologia: values[1]
+                })
+              })
+          }
         })
+    })
+    .then(_ => {
+      res.json(toReturn)
+      driver.quit();
+      var end = new Date().getTime();
+      var time = end - start;
+      console.log('Execution time: ' + time);
     })
 
 });
