fe4b0f7e4a053da39bacfa2ee9db072b34c5dedf
lpeppe <lcpp1994@gmail.com>
2017-08-01 15:41:11 +0200

    added init function

--- qcScraper.js
+++ qcScraper.js
@@ -6,6 +6,7 @@ var webdriver = require('selenium-webdriver'),
   until = webdriver.until;
 const myCache = new NodeCache();
 var app = express();
+init();
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -20,65 +21,70 @@ app.get('/', function(req, res) {
     var time = end - start;
     console.log('Execution time: ' + time);
   } catch (err) {
-    var driver = new webdriver.Builder()
-      .forBrowser('phantomjs')
-      .build();
-    var toReturn = {};
-    var names = [];
-    var links = [];
-    driver.get('http://www.quattrocalici.it/ricerca/denominazioni');
-    driver.findElement(By.css('#td-outer-wrap > div.td-main-content-wrap.td-container-wrap > div > div.td-pb-row.body-content > div.td-pb-span8.td-main-content > div.wpb_raw_code.wpb_content_element.wpb_raw_html.vc_custom_1493908674521.wine-list > div > div.bootstrap-table > div.fixed-table-container > div.fixed-table-pagination > div.pull-left.pagination-detail > span.page-list > span > button'))
-      .then(element => {
-        driver.wait(until.elementIsVisible(element));
-        tryClick(element);
-        driver.findElement(By.css('#td-outer-wrap > div.td-main-content-wrap.td-container-wrap > div > div.td-pb-row.body-content > div.td-pb-span8.td-main-content > div.wpb_raw_code.wpb_content_element.wpb_raw_html.vc_custom_1493908674521.wine-list > div > div.bootstrap-table > div.fixed-table-container > div.fixed-table-pagination > div.pull-left.pagination-detail > span.page-list > span > ul > li:nth-child(4) > a'))
-          .then(elem => {
-            driver.wait(until.elementIsVisible(elem));
-            tryClick(elem);
-            driver.findElement(By.css('#td-outer-wrap > div.td-main-content-wrap.td-container-wrap > div > div.td-pb-row.body-content > div.td-pb-span8.td-main-content > div.wpb_raw_code.wpb_content_element.wpb_raw_html.vc_custom_1493908674521.wine-list > div > div.bootstrap-table > div.fixed-table-container > div.fixed-table-body > table > thead > tr > th:nth-child(3) > div.th-inner.sortable'))
-              .then(regione => {
-                tryClick(regione);
-                scrape();
+    init();
+  }
+});
+
+function init() {
+  var start = new Date().getTime();
+  var driver = new webdriver.Builder()
+    .forBrowser('phantomjs')
+    .build();
+  var toReturn = {};
+  var names = [];
+  var links = [];
+  driver.get('http://www.quattrocalici.it/ricerca/denominazioni');
+  driver.findElement(By.css('#td-outer-wrap > div.td-main-content-wrap.td-container-wrap > div > div.td-pb-row.body-content > div.td-pb-span8.td-main-content > div.wpb_raw_code.wpb_content_element.wpb_raw_html.vc_custom_1493908674521.wine-list > div > div.bootstrap-table > div.fixed-table-container > div.fixed-table-pagination > div.pull-left.pagination-detail > span.page-list > span > button'))
+    .then(element => {
+      driver.wait(until.elementIsVisible(element));
+      tryClick(element);
+      driver.findElement(By.css('#td-outer-wrap > div.td-main-content-wrap.td-container-wrap > div > div.td-pb-row.body-content > div.td-pb-span8.td-main-content > div.wpb_raw_code.wpb_content_element.wpb_raw_html.vc_custom_1493908674521.wine-list > div > div.bootstrap-table > div.fixed-table-container > div.fixed-table-pagination > div.pull-left.pagination-detail > span.page-list > span > ul > li:nth-child(4) > a'))
+        .then(elem => {
+          driver.wait(until.elementIsVisible(elem));
+          tryClick(elem);
+          driver.findElement(By.css('#td-outer-wrap > div.td-main-content-wrap.td-container-wrap > div > div.td-pb-row.body-content > div.td-pb-span8.td-main-content > div.wpb_raw_code.wpb_content_element.wpb_raw_html.vc_custom_1493908674521.wine-list > div > div.bootstrap-table > div.fixed-table-container > div.fixed-table-body > table > thead > tr > th:nth-child(3) > div.th-inner.sortable'))
+            .then(regione => {
+              tryClick(regione);
+              scrape();
+            })
+        })
+    })
+
+  function scrape() {
+    driver.findElements(By.css('#td-outer-wrap > div.td-main-content-wrap.td-container-wrap > div > div.td-pb-row.body-content > div.td-pb-span8.td-main-content > div.wpb_raw_code.wpb_content_element.wpb_raw_html.vc_custom_1493908674521.wine-list > div > div.bootstrap-table > div.fixed-table-container > div.fixed-table-body > table > tbody > tr'))
+      .then(elems => {
+        for (var i in elems)
+          grabData(elems[i])
+        driver.findElement(By.css('#td-outer-wrap > div.td-main-content-wrap.td-container-wrap > div > div.td-pb-row.body-content > div.td-pb-span8.td-main-content > div.wpb_raw_code.wpb_content_element.wpb_raw_html.vc_custom_1493908674521.wine-list > div > div.bootstrap-table > div.fixed-table-container > div.fixed-table-pagination > div.pull-right.pagination > ul > li.page-next'))
+          .then(li => {
+            li.getAttribute("class")
+              .then(cl => {
+                if (cl.indexOf("disabled") == -1) {
+                  li.findElement(By.css('a'))
+                    .then(anchor => {
+                      driver.wait(until.elementIsVisible(anchor));
+                      tryClick(anchor);
+                      scrape();
+                    })
+                } else {
+                  myCache.set("data", toReturn);
+                  driver.quit();
+                  var end = new Date().getTime();
+                  var time = end - start;
+                  console.log('Execution time: ' + time);
+                }
               })
           })
+
       })
+  }
 
-    function scrape() {
-      driver.findElements(By.css('#td-outer-wrap > div.td-main-content-wrap.td-container-wrap > div > div.td-pb-row.body-content > div.td-pb-span8.td-main-content > div.wpb_raw_code.wpb_content_element.wpb_raw_html.vc_custom_1493908674521.wine-list > div > div.bootstrap-table > div.fixed-table-container > div.fixed-table-body > table > tbody > tr'))
-        .then(elems => {
-          for (var i in elems)
-            grabData(elems[i])
-          driver.findElement(By.css('#td-outer-wrap > div.td-main-content-wrap.td-container-wrap > div > div.td-pb-row.body-content > div.td-pb-span8.td-main-content > div.wpb_raw_code.wpb_content_element.wpb_raw_html.vc_custom_1493908674521.wine-list > div > div.bootstrap-table > div.fixed-table-container > div.fixed-table-pagination > div.pull-right.pagination > ul > li.page-next'))
-            .then(li => {
-              li.getAttribute("class")
-                .then(cl => {
-                  if (cl.indexOf("disabled") == -1) {
-                    li.findElement(By.css('a'))
-                      .then(anchor => {
-                        driver.wait(until.elementIsVisible(anchor));
-                        tryClick(anchor);
-                        scrape();
-                      })
-                  } else {
-                    myCache.set("data", toReturn);
-                    driver.quit();
-                    var end = new Date().getTime();
-                    var time = end - start;
-                    console.log('Execution time: ' + time);
-                  }
-                })
-            })
-
-        })
-    }
-
-    function tryClick(elem) {
-      elem.click()
-        .then(null, _ => {
-          driver.sleep(1000);
-          tryClick(elem)
-        })
-    }
+  function tryClick(elem) {
+    elem.click()
+      .then(null, _ => {
+        driver.sleep(1000);
+        tryClick(elem)
+      })
   }
 
   function grabData(data) {
@@ -111,7 +117,7 @@ app.get('/', function(req, res) {
           })
       })
   }
-});
+}
 
 app.get('/page', function(req, res) {
   request(req.query.link, function(error, response, body) {
