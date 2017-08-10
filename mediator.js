ffba2bf1cb94695c2941c6806734989e72bf9185
lpeppe <lcpp1994@gmail.com>
2017-08-10 18:13:13 +0200

    minor bugfix

--- mediator.js
+++ mediator.js
@@ -2,43 +2,44 @@ var express = require('express');
 var request = require('request');
 var app = express();
 
+var myIp = 'http://ec2-52-36-14-51.us-west-2.compute.amazonaws.com';
 
 app.get('/ris', function(req, res) {
-  res.redirect('http://localhost:8082?lat=' + req.query.lat + '&lng=' + req.query.lng + '&radius=' + req.query.radius);
+  res.redirect(myIp + ':8082?lat=' + req.query.lat + '&lng=' + req.query.lng + '&radius=' + req.query.radius);
 })
 
 app.get('/risdescr', function(req, res) {
-  res.redirect('http://localhost:8082/descr?placeid=' + req.query.placeid);
+  res.redirect(myIp + ':8082/descr?placeid=' + req.query.placeid);
 })
 
 app.get('/placedet', function(req, res) {
-  res.redirect('http://localhost:8081/?placeid=' + req.query.placeid);
+  res.redirect(myIp + ':8081/?placeid=' + req.query.placeid);
 })
 
 app.get('/placedetrev', function(req, res) {
-  res.redirect('http://localhost:8081/reverse?lat=' + req.query.lat + "&lng=" + req.query.lng);
+  res.redirect(myIp + ':8081/reverse?lat=' + req.query.lat + "&lng=" + req.query.lng);
 })
 
 app.get('/qc', function(req, res) {
-  res.redirect('http://localhost:8084/');
+  res.redirect(myIp + ':8084/');
 })
 
 app.get('/qcpage', function(req, res) {
-  res.redirect('http://localhost:8084/page?link=' + req.query.link);
+  res.redirect(myIp + ':8084/page?link=' + req.query.link);
 })
 
 app.get('/taattr', function(req, res) {
   if (req.query.placeid == undefined)
-    res.redirect('http://localhost:8080?loc=' + req.query.loc);
+    res.redirect(myIp + ':8080?loc=' + req.query.loc);
   else
-    res.redirect('http://localhost:8080?placeid=' + req.query.placeid + '&page=' + req.query.page);
+    res.redirect(myIp + ':8080?placeid=' + req.query.placeid + '&page=' + req.query.page);
 })
 
 app.get('/tarev', function(req, res) {
   if (req.query.risid == undefined)
-    res.redirect('http://localhost:8083?ris=' + req.query.ris + '&citta=' + req.query.citta);
+    res.redirect(myIp + ':8083?ris=' + req.query.ris + '&citta=' + req.query.citta);
   else
-    res.redirect('http://localhost:8083?placeid=' + req.query.placeid + '&risid=' + req.query.risid + '&page=' + req.query.page);
+    res.redirect(myIp + ':8083?placeid=' + req.query.placeid + '&risid=' + req.query.risid + '&page=' + req.query.page);
 })
 
 app.listen(8090, function() {
